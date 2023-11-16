922ffb2de295083bab0343577268a65fc33c1bed
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-17 02:01:20 +0500

    Register Front Functionality

--- club/views.py
+++ club/views.py
@@ -5,3 +5,13 @@ def home_view(request):
     page_header = "Home"
     context = {"page_header": page_header}
     return render(request, "user_interface/home.html", context)
+
+def register_view(request):
+    page_header = "Register"
+    
+    if request.method == "POST":
+        pass
+
+    context = {"page_header": page_header}
+    return render(request, "auth/register.html", context)
+
