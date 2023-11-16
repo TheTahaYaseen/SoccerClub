f63040d4de5638038732d0ab4ac078a4d4e68262
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-17 02:40:07 +0500

    Very Proper Precise Error Handling

---
+++ club/functions/auth.py
@@ -0,0 +1,59 @@
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+import phonenumbers
+
+from django.contrib.auth.models import User
+from ..models import UserProfile
+
+def email_validation(email):    
+    try:
+        validate_email(email)
+        return True
+    except ValidationError:
+        return False
+
+def phone_number_validation(phone_number):
+    try:
+        parsed_number = phonenumbers.parse(phone_number, "PK")
+        is_valid = phonenumbers.is_valid_number(parsed_number)
+        return is_valid
+    except phonenumbers.NumberParseException:
+        return False
+
+def username_already_used(username):
+    if User.objects.filter(username=username).exists():
+        return True
+    else:
+        return False
+
+def email_already_used(email):
+    if UserProfile.objects.filter(email=email).exists():
+        return True
+    else:
+        return False
+
+def validate_credentials(username, email, phone_number, password, password_confirmation):
+    error = ""
+    if not username:
+        error = "Username Cannot Be Empty!"
+    elif not email:
+        error = "Email Cannot Be Empty!"
+    elif not phone_number:
+        error = "Phone Number Cannot Be Empty!"
+    elif not password:
+        error = "Password Cannot Be Empty!"
+    elif not password_confirmation:
+        error = "Password Confirmation Cannot Be Empty!"
+    elif password != password_confirmation:
+        error = "Password And Password Confirmation Must Be Same!"
+    elif not email_validation(email):
+        error = "Please Use A Valid Email!"
+    elif not phone_number_validation(phone_number):
+        error = "Please Use A Valid Phone Number!"
+    elif username_already_used(username):
+        error = "Username Already Used!"
+    elif email_already_used(email):
+        error = "Email Already Used!"
+
+    return error
--- club/views.py
+++ club/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .functions.auth import validate_credentials
+
 # Create your views here.
 def home_view(request):
     page_header = "Home"
@@ -7,11 +9,29 @@ def home_view(request):
     return render(request, "user_interface/home.html", context)
 
 def register_view(request):
+    
     page_header = "Register"
+    error = ""
+    
+    username = ""
+    email = ""
+    phone_number = ""
+    password = ""
+    password_confirmation = ""
     
     if request.method == "POST":
-        pass
+        
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        phone_number = request.POST.get("phone_number")
+        password = request.POST.get("password")
+        password_confirmation = request.POST.get("password_confirmation")
 
-    context = {"page_header": page_header}
+        error = validate_credentials(username, email, phone_number, password, password_confirmation)
+
+    context = {"page_header": page_header, 
+               "error": error, 
+               "username": username, "email": email, "phone_number": phone_number, 
+               "password": password, "password_confirmation": password_confirmation}
     return render(request, "auth/register.html", context)
 
