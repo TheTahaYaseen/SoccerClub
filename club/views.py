71cf9ad96533a1dca9277eed03953cdf214ac28d
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-17 01:54:21 +0500

    Properly Integrating And Testing Static Stuff

--- club/views.py
+++ club/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    context = {}
+    page_header = "Home"
+    context = {"page_header": page_header}
     return render(request, "user_interface/home.html", context)
