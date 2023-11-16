841f5f86cef6ca2887087c5ac8754b45dc02fc71
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-16 19:54:34 +0500

    Minor Refactoring

--- soccer_club/asgi.py
+++ soccer_club/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SoccerClub project.
+ASGI config for soccer_club project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoccerClub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soccer_club.settings')
 
 application = get_asgi_application()
--- soccer_club/wsgi.py
+++ soccer_club/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SoccerClub project.
+WSGI config for soccer_club project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoccerClub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soccer_club.settings')
 
 application = get_wsgi_application()
